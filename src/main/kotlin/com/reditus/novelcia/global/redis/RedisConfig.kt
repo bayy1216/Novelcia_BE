e04024c4f337be6a3bda5ffccb026c95690db687
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-07-22 12:38:31 +0900

    [Feat]: 레디스 connectionFactory auto-config로 변경

--- src/main/kotlin/com/reditus/novelcia/global/redis/RedisConfig.kt
+++ src/main/kotlin/com/reditus/novelcia/global/redis/RedisConfig.kt
@@ -1,6 +1,5 @@
 package com.reditus.novelcia.global.redis
 
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
@@ -9,23 +8,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig(
-    private val redisProperties: RedisProperties,
 ) {
 
     @Bean
-    fun redisTemplate(): RedisTemplate<String, String> {
+    fun redisTemplate(
+        redisConnectionFactory: LettuceConnectionFactory
+    ): RedisTemplate<String, String> {
         val redisTemplate = RedisTemplate<String, String>()
-        redisTemplate.connectionFactory = redisConnectionFactory()
+        redisTemplate.connectionFactory = redisConnectionFactory
         redisTemplate.keySerializer = StringRedisSerializer()
         redisTemplate.valueSerializer = StringRedisSerializer()
         return redisTemplate
     }
-
-    @Bean
-    fun redisConnectionFactory(): LettuceConnectionFactory {
-        return LettuceConnectionFactory(redisProperties.host, redisProperties.port)
-    }
-
 
 
 }
