e7e28f5a39a13efe6bcb3124f5821fe1e067755d
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-07-22 20:37:25 +0900

    [Feat]: 에피소드 스웨거 설정

--- src/main/kotlin/com/reditus/novelcia/interfaces/episode/EpisodeController.kt
+++ src/main/kotlin/com/reditus/novelcia/interfaces/episode/EpisodeController.kt
@@ -5,6 +5,7 @@ import com.reditus.novelcia.domain.episode.EpisodeQueryService
 import com.reditus.novelcia.domain.episode.EpisodeService
 import com.reditus.novelcia.domain.episode.port.EpisodePagingSort
 import com.reditus.novelcia.global.security.LoginUserDetails
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.PageRequest
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -20,17 +21,19 @@ class EpisodeController(
     private val episodeService: EpisodeService,
     private val episodeQueryService: EpisodeQueryService,
 ) {
+    @Operation(summary = "에피소드 목록 조회")
     @GetMapping("/api/novels/{novelId}/episodes")
     fun getEpisodesOffsetPaging(
         @PathVariable novelId: Long,
-        sort: EpisodePagingSort,
-        page: Int,
-        size: Int,
+        sort: EpisodePagingSort = EpisodePagingSort.CREATED_AT_DESC,
+        page: Int = 0,
+        size: Int = 20,
     ): List<EpisodeModel.Meta> {
         val models = episodeQueryService.getEpisodeModelsByOffsetPaging(novelId, PageRequest.of(page, size), sort)
         return models
     }
 
+    @Operation(summary = "에피소드 상세 조회")
     @GetMapping("/api/episodes/{episodeId}")
     fun getEpisodeDetail(
         @AuthenticationPrincipal loginUserDetails: LoginUserDetails,
@@ -42,6 +45,7 @@ class EpisodeController(
         )
     }
 
+    @Operation(summary = "에피소드 생성", description = "에피소드 생성 후 ID 반환")
     @PostMapping("/api/novels/{novelId}/episodes")
     fun createEpisode(
         @PathVariable novelId: Long,
