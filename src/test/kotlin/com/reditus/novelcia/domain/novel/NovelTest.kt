3b6f5b8be8acbc1ffe3d466f5248cdea10af7398
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-07-26 16:49:47 +0900

    [Test]: test 하드코딩 제거

--- src/test/kotlin/com/reditus/novelcia/domain/novel/NovelTest.kt
+++ src/test/kotlin/com/reditus/novelcia/domain/novel/NovelTest.kt
@@ -9,31 +9,34 @@ class NovelTest {
 
     @Test
     fun `update시 tag들의 PUT 연산이 정상적으로 작동한다`() {
-        // given
+        // given (novel에는 tag1, tag2가 있고, command에는 tag1, tag3, tag4가 있다)
         val user = User.fixture()
         val tag1 = Tag.fixture("태그1")
         val tag2 = Tag.fixture("태그2")
+        val tag3 = Tag.fixture("태그3")
+        val tag4 = Tag.fixture("태그4")
         val novel = Novel.fixture(author=user)
 
         novel.addNovelAndTag(tag1)
         novel.addNovelAndTag(tag2)
 
-        // when
-
         val command: NovelCommand.Update = NovelCommand.Update(
             title = "수정된 제목",
             description = "수정된 설명",
             thumbnailImageUrl = "수정된 이미지",
-            tagNames = listOf("태그1","태그3", "태그4")
+            tagNames = listOf(tag1.name, tag3.name, tag4.name)
         )
-        val commandTags: Set<Tag> = setOf(tag1,Tag.fixture("태그3"), Tag.fixture("태그4"))
+        val commandTags: Set<Tag> = setOf(tag1,tag3, tag4)
+
+        // when
         novel.update(command, commandTags)
 
-        // then
+        // then (PUT 연산에 의해서 tag2는 삭제되고, tag3, tag4가 추가되어,
+        // 결국 novel에는 tag1, tag3, tag4만 남아있어야 한다)
         assertEquals("수정된 제목", novel.title)
         assertEquals("수정된 설명", novel.description)
         assertEquals("수정된 이미지", novel.thumbnailImageUrl)
         assertEquals(3, novel.tags.size)
-        assertEquals(novel.novelAndTags.map { it.tag.name }.toSet(), setOf("태그1","태그3", "태그4"))
+        assertEquals(novel.novelAndTags.map { it.tag.name }.toSet(), setOf(tag1.name, tag3.name, tag4.name))
     }
 }
