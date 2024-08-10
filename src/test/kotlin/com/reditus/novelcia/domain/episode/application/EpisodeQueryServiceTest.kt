cf550cd1078f1e1ff784ae9ff55218a543c47099
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-08-10 18:16:39 +0900

    [Fix]: 테스트 충돌 해소

--- src/test/kotlin/com/reditus/novelcia/domain/episode/application/EpisodeQueryServiceTest.kt
+++ src/test/kotlin/com/reditus/novelcia/domain/episode/application/EpisodeQueryServiceTest.kt
@@ -1,11 +1,14 @@
 package com.reditus.novelcia.domain.episode.application
 
 import com.reditus.novelcia.domain.LoginUserId
+import com.reditus.novelcia.domain.episode.Episode
 import com.reditus.novelcia.domain.episode.EpisodeCommand
 import com.reditus.novelcia.domain.episode.port.EpisodePagingSort
 import com.reditus.novelcia.domain.novel.Novel
 import com.reditus.novelcia.domain.novel.ReadAuthority
 import com.reditus.novelcia.domain.user.User
+import com.reditus.novelcia.global.util.newTransaction
+import com.reditus.novelcia.global.util.transactional
 import com.reditus.novelcia.infrastructure.episode.EpisodeRepository
 import com.reditus.novelcia.infrastructure.findByIdOrThrow
 import com.reditus.novelcia.infrastructure.novel.NovelRepository
@@ -17,9 +20,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.domain.PageRequest
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import org.springframework.test.annotation.Rollback
+import org.springframework.transaction.annotation.Transactional
 import java.util.concurrent.TimeUnit
 
 @SpringBootTest
+@Transactional
 class EpisodeQueryServiceTest @Autowired constructor(
     private val episodeService: EpisodeService,
     private val episodeQueryService: EpisodeQueryService,
@@ -30,23 +36,29 @@ class EpisodeQueryServiceTest @Autowired constructor(
 ) {
 
     @Test
-    fun `에피소드 상세조회를 n번 할시, EpisodeModel_Meta의 조회수가 정상적으로 n만큼 증가한다`() {
+    @Rollback
+    fun `에피소드 상세조회를 n번 할시, EpisodeModel_Meta의 조회수가 정상적으로 n만큼 증가한다`(){
         // given
-        val user = userRepository.save(User.fixture())
-        val novel = novelRepository.save(Novel.fixture(
-            author = user,
-        ))
-        val episodeId = episodeService.createEpisode(
-            userId = LoginUserId(user.id),
-            novelId = novel.id,
-            command = EpisodeCommand.Create(
-                title = "title",
-                content = "content",
-                authorComment = "authorComment",
-                readAuthority = ReadAuthority.FREE,
+        lateinit var user: User
+        lateinit var novel: Novel
+        lateinit var episode: Episode
+        newTransaction {
+            user = userRepository.save(User.fixture())
+            novel = novelRepository.save(Novel.fixture(
+                author = user,
+            ))
+            val episodeId = episodeService.createEpisode(
+                userId = LoginUserId(user.id),
+                novelId = novel.id,
+                command = EpisodeCommand.Create(
+                    title = "title",
+                    content = "content",
+                    authorComment = "authorComment",
+                    readAuthority = ReadAuthority.FREE,
+                )
             )
-        )
-        val episode = episodeRepository.findByIdOrThrow(episodeId)
+            episode = episodeRepository.findByIdOrThrow(episodeId)
+        }
 
         // when
         repeat(5) {
