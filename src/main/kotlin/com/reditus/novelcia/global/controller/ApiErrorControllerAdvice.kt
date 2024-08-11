4fcbc4497f8bb4ca69a6757456af84c04e0f301b
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-08-11 20:26:14 +0900

    [Feat]: API 에러처리 추가

--- src/main/kotlin/com/reditus/novelcia/global/controller/ApiErrorControllerAdvice.kt
+++ src/main/kotlin/com/reditus/novelcia/global/controller/ApiErrorControllerAdvice.kt
@@ -3,15 +3,111 @@ package com.reditus.novelcia.global.controller
 import com.reditus.novelcia.global.exception.ElementConflictException
 import com.reditus.novelcia.global.exception.NoPermissionException
 import com.reditus.novelcia.global.exception.NotAuthorizationException
+import com.reditus.novelcia.global.security.LoginUserDetails
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.HttpRequestMethodNotSupportedException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.MissingPathVariableException
+import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.method.annotation.HandlerMethodValidationException
+import org.springframework.web.multipart.support.MissingServletRequestPartException
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
 class ApiErrorControllerAdvice {
     private val log = LoggerFactory.getLogger(this::class.java)
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun methodArgumentNotValidException(e: MethodArgumentNotValidException): ApiResponse<Unit> {
+        log.info("MethodArgumentNotValidException", e)
+        return ApiResponse.fail(
+            message = e.bindingResult.fieldError?.defaultMessage ?: "COMMON-VALIDATION-EXCEPTION",
+            errorCode = "COMMON-VALIDATION",
+        )
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleHttpMessageNotReadable(e: HttpMessageNotReadableException): ApiResponse<Unit> {
+        log.info("HttpMessageNotReadableException", e)
+        return ApiResponse.fail(
+            message = "COMMON-INVALID-REQUEST-EXCEPTION",
+            errorCode = "COMMON-INVALID-REQUEST",
+        )
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    fun handleHttpRequestMethodNotSupported(e: HttpRequestMethodNotSupportedException): ApiResponse<Unit> {
+        log.info("HttpRequestMethodNotSupportedException", e)
+        return ApiResponse.fail(
+            message = "COMMON-METHOD-NOT-SUPPORTED-EXCEPTION",
+            errorCode = "COMMON-METHOD-NOT-SUPPORTED",
+        )
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleMissingServletRequestPart(e: MissingServletRequestPartException): ApiResponse<Unit> {
+        log.info("MissingServletRequestPartException", e)
+        return ApiResponse.fail(
+            message = "COMMON-MISSING-PART-EXCEPTION",
+            errorCode = "COMMON-MISSING-PART",
+        )
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleMissingServletRequestParameter(e: MissingServletRequestParameterException): ApiResponse<Unit> {
+        log.info("MissingServletRequestParameterException", e)
+        return ApiResponse.fail(
+            message = "COMMON-MISSING-PARAMETER-EXCEPTION",
+            errorCode = "COMMON-MISSING-PARAMETER",
+        )
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleMissingPathVariable(e: MissingPathVariableException): ApiResponse<Unit> {
+        log.info("MissingPathVariableException", e)
+        return ApiResponse.fail(
+            message = "COMMON-MISSING-PATH-VARIABLE-EXCEPTION",
+            errorCode = "COMMON-MISSING-PATH-VARIABLE",
+        )
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleHttpMediaTypeNotSupported(e: HttpMediaTypeNotSupportedException): ApiResponse<Unit> {
+        log.info("HttpMediaTypeNotSupportedException", e)
+        return ApiResponse.fail(
+            message = "COMMON-MEDIA-TYPE-NOT-SUPPORTED-EXCEPTION",
+            errorCode = "COMMON-MEDIA-TYPE-NOT-SUPPORTED",
+        )
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleHandlerMethodValidationException(e: HandlerMethodValidationException): ApiResponse<Unit> {
+        log.info("HandlerMethodValidationException", e)
+        return ApiResponse.fail(
+            message = e.message,
+            errorCode = "COMMON-VALIDATION",
+        )
+    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -74,11 +170,17 @@ class ApiErrorControllerAdvice {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun runtimeException(e: RuntimeException): ApiResponse<Unit> {
-        log.error("RuntimeException", e)
+    fun runtimeException(
+        e: RuntimeException,
+        @AuthenticationPrincipal
+        loginUserDetails: LoginUserDetails?,
+    ): ApiResponse<Unit> {
+        val eventId = MDC.get(CommonHttpRequestInterceptor.HEADER_REQUEST_UUID_KEY)
+        val loginUserId = loginUserDetails?.loginUserId?.value
+        log.error("RuntimeException, eventId = {}, loginUserId = {}", eventId, loginUserId, e)
         return ApiResponse.fail(
             message = "서버에서 오류가 발생했습니다. 잠시 후 다시 시도해주세요.",
-            errorCode = "COMMON-RUNTIME-EXCEPTION",
+            errorCode = "event:$eventId",
         )
     }
 }
