bf9ed2c6a96848f0ad669c826d6c774ce605f3ff
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-08-13 01:00:02 +0900

    [Feat]: 배치 Repository

---
+++ src/main/kotlin/com/reditus/novelcia/infrastructure/BatchRepository.kt
@@ -0,0 +1,94 @@
+package com.reditus.novelcia.infrastructure
+
+import jakarta.persistence.EntityManager
+import jakarta.persistence.EntityManagerFactory
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.support.JpaMetamodelEntityInformation
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository
+import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+
+
+@NoRepositoryBean
+interface BatchRepository<T, ID>: JpaRepository<T, ID> {
+    fun saveInBatch(entities: List<T>)
+}
+
+@Transactional(propagation = Propagation.NEVER)
+class BatchRepositoryImpl<T, ID>(
+    jpaMetamodelEntityInformation: JpaMetamodelEntityInformation<T, ID>,
+    entityManager: EntityManager,
+) : SimpleJpaRepository<T, ID>(
+    jpaMetamodelEntityInformation,
+    entityManager
+), BatchRepository<T, ID> {
+    override fun saveInBatch(entities: List<T>) {
+        val batchExecutor = SpringContext.getBean(BatchExecutor::class.java)
+        batchExecutor.saveInBatch(entities)
+    }
+}
+
+@Component
+class BatchExecutor(
+    private val entityManagerFactory: EntityManagerFactory
+) {
+//    @Value("\${spring.jpa.properties.hibernate.jdbc.batch_size}")
+    private val batchSize = 30
+
+    fun <T> saveInBatch(entities: List<T>) {
+        val entityManager = entityManagerFactory.createEntityManager()
+        val entityTransaction = entityManager.transaction
+
+        try {
+            entityTransaction.begin()
+
+            for ((i, entity) in entities.withIndex()) {
+                if (i % batchSize == 0 && i > 0) {
+                    logger.info("Flushing the entity manager ...")
+                    entityTransaction.commit()
+                    entityTransaction.begin()
+
+                    entityManager.clear()
+                }
+
+                entityManager.persist(entity)
+            }
+
+            logger.info("Flushing the remaining entities ...")
+
+            entityTransaction.commit()
+        } catch (e: RuntimeException) {
+            if (entityTransaction.isActive) {
+                entityTransaction.rollback()
+            }
+            throw e
+        } finally {
+            entityManager.close()
+        }
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(BatchExecutor::class.java)
+    }
+}
+
+@Component
+class SpringContext : ApplicationContextAware {
+
+    companion object {
+        lateinit var context: ApplicationContext
+
+        fun <T : Any?> getBean(beanClass: Class<T>): T {
+            return context.getBean(beanClass)
+        }
+    }
+
+    override fun setApplicationContext(applicationContext: ApplicationContext) {
+        context = applicationContext
+    }
+}
--- src/main/kotlin/com/reditus/novelcia/infrastructure/JpaConfig.kt
+++ src/main/kotlin/com/reditus/novelcia/infrastructure/JpaConfig.kt
@@ -5,9 +5,11 @@ import jakarta.persistence.EntityManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(repositoryBaseClass = BatchRepositoryImpl::class)
 class JpaConfig(
     private val em: EntityManager
 ) {
